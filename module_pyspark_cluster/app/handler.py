bf1fcf2145fd24c7fc4dce890a9850ba6f4f0c31
Vuong Nguyen <vugn@future-demand.com>
2025-07-02 23:38:35 +0200

    new: add app contains scripts by docker exec -it listen_brainz_spark_master spark-submit /opt/bitnami/spark/app/hello.py

---
+++ module_pyspark_cluster/app/handler.py
@@ -0,0 +1,36 @@
+from minio import Minio
+from minio.error import S3Error
+import os
+
+
+def upload_file_to_minio(local_file_path, bucket_name, object_name):
+    # Connect to MinIO server
+    client = Minio(
+        endpoint=os.getenv("MINIO_ENDPOINT", "localhost:9000"),
+        access_key=os.getenv("MINIO_ACCESS_KEY", "admin"),
+        secret_key=os.getenv("MINIO_SECRET_KEY", "password"),
+        secure=False
+    )
+
+    # Create bucket if not exists
+    found = client.bucket_exists(bucket_name)
+    if not found:
+        client.make_bucket(bucket_name)
+        print(f"Bucket '{bucket_name}' created.")
+    else:
+        print(f"Bucket '{bucket_name}' already exists.")
+
+    # Upload the file
+    try:
+        client.fput_object(bucket_name, object_name, local_file_path)
+        print(f"File '{local_file_path}' uploaded as '{object_name}' in bucket '{bucket_name}'.")
+    except S3Error as err:
+        print(f"Failed to upload: {err}")
+
+
+if __name__ == "__main__":
+    local_file = "static_files/dataset.json"  # adjust path to your static file
+    bucket = "ingestion-bucket"
+    object_name = "dataset.json"  # name inside the bucket
+
+    upload_file_to_minio(local_file, bucket, object_name)
---
+++ module_pyspark_cluster/app/hello.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+
+
+def main():
+    master_url = "spark://spark-master:7077"
+    app_name = "HelloWorld"
+
+    # Initialize SparkSession
+    spark = SparkSession.builder.appName(app_name).master(master_url).getOrCreate()
+
+    # Create an RDD containing numbers from 1 to 1000
+    numbers_rdd = spark.sparkContext.parallelize(range(1, 1000))
+
+    # Count the elements in the RDD
+    count = numbers_rdd.count()
+
+    print(f"Count of numbers from 1 to 1000 is: {count}")
+
+    # Stop the SparkSession
+    spark.stop()
+
+
+if __name__ == "__main__":
+    main()
