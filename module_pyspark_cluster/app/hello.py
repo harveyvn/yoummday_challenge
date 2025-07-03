efecb77777fa183948b6fb1b2fe022152efbc611
Vuong Nguyen <vugn@future-demand.com>
2025-07-03 15:19:21 +0200

    fix: remove comment and mount app/ to docker

--- module_pyspark_cluster/app/hello.py
+++ module_pyspark_cluster/app/hello.py
@@ -1,24 +1,21 @@
 import os
 from pyspark.sql import SparkSession
+from utils.logging import log
 
 
 def main():
     master_url = os.getenv("SPARK_MASTER_URL", "local[*]")
     app_name = "HelloWorld"
 
-    # Initialize SparkSession
     spark = SparkSession.builder.appName(app_name).master(master_url).getOrCreate()
-
-    # Create an RDD containing numbers from 1 to 1000
     numbers_rdd = spark.sparkContext.parallelize(range(1, 1000))
 
-    # Count the elements in the RDD
     count = numbers_rdd.count()
 
-    print(f"Count of numbers from 1 to 1000 is: {count}")
+    log.info(f"Count of numbers from 1 to 1000 is: {count}")
 
-    # Stop the SparkSession
     spark.stop()
+    log.info(f"Stopped SparkSession")
 
 
 if __name__ == "__main__":
