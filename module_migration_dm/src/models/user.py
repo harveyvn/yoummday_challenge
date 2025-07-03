e82ffd2d93b86b024fadd1423b3d7885cb34feb1
Vuong Nguyen <vugn@future-demand.com>
2025-07-04 00:53:25 +0200

    new: add last updated and date created to users

--- module_migration_dm/src/models/user.py
+++ module_migration_dm/src/models/user.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime, func, TIMESTAMP
 from .base import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(String, primary_key=True, nullable=False)
     user_name = Column(String(255), unique=True, nullable=False)
+
+    date_created = Column(DateTime(timezone=False), server_default=func.now())
+    last_updated = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
 
     def __repr__(self):
         return f"<User(id={self.id}, user_name='{self.user_name}')>"
