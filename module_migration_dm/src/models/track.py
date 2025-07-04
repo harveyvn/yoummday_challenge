1fcebad546189b5f892b5a170860ec2096d7c676
Vuong Nguyen <vugn@future-demand.com>
2025-07-04 18:57:03 +0200

    fix: update track model with release null and text for other names

--- module_migration_dm/src/models/track.py
+++ module_migration_dm/src/models/track.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime, func, TIMESTAMP
+from sqlalchemy import Column, String, DateTime, func, TIMESTAMP, Text
 from sqlalchemy.dialects.postgresql import UUID
 from .base import Base
 
@@ -7,21 +7,14 @@ class Track(Base):
     __tablename__ = "tracks"
 
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
-    track_name = Column(String(255), nullable=False)
-    artist_name = Column(String(255), nullable=False)
-    release_name = Column(String(255), nullable=False)
+    track_name = Column(Text(), nullable=False)
+    artist_name = Column(Text(), nullable=False)
+    release_name = Column(Text(), nullable=True)
 
     recording_msid = Column(UUID(as_uuid=True), nullable=False)
     artist_msid = Column(UUID(as_uuid=True), nullable=False)
-    release_msid = Column(UUID(as_uuid=True), nullable=False)
+    release_msid = Column(UUID(as_uuid=True), nullable=True)
 
     date_created = Column(DateTime(timezone=False), server_default=func.now())
     last_updated = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
 
-    def __repr__(self):
-        return (
-            f"<Track(id={self.track_id}, "
-            f"track_name='{self.track_name}', "
-            f"artist_name='{self.artist_name}', "
-            f"recording_msid='{self.recording_msid}')>"
-        )
