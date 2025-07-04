884e86af5e2ee96311396f72716511c31dcb8047
Vuong Nguyen <vugn@future-demand.com>
2025-07-04 10:33:38 +0200

    feat: create docker container to do migration automatically

--- module_migration_dm/src/config.py
+++ module_migration_dm/src/config.py
@@ -1,15 +1,9 @@
 import os
-from dotenv import load_dotenv
 
-if os.path.exists(".encrypted_dm_env") is False:
-    load_dotenv(dotenv_path="src/.encrypted_dm_env")
-else:
-    load_dotenv(dotenv_path=".encrypted_dm_env")
-
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER", "admin")
+DB_PASS = os.getenv("DB_PASS", "password")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5443")
+DB_NAME = os.getenv("DB_NAME", "domain_model_db")
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
