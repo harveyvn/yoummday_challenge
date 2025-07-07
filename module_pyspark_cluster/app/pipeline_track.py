67ce4315119f1cc4efbb12f8aeae3609c99642ca
Vuong Nguyen <vugn@future-demand.com>
2025-07-07 22:24:18 +0200

    fix: update data validation tool by removing null track

--- module_pyspark_cluster/app/pipeline_track.py
+++ module_pyspark_cluster/app/pipeline_track.py
@@ -10,22 +10,24 @@ from app.utils.sql_utils import insert_on_conflict_do_update
 class Track:
     def __init__(self, bucket_name: str, s3_path: str):
         self._df, self._spark = get_data(bucket_name, s3_path)
+        self._df = self._df.toDF("user_name", "listened_at", "musicbrainz-artist-id", "artist-name", "musicbrainz-track-id", "track-name")
         self._s3_transform_path: str = f"s3a://{bucket_name}/transformed/{datetime.today().strftime('%Y%m%d')}/tracks/"
         self._df_tracks: DataFrame = None
 
     def _transform(self):
+        self._df.show(2)
         df_tracks = self._df.select(
-            col("track_metadata.track_name").alias("track_name"),
-            col("track_metadata.artist_name").alias("artist_name"),
-            col("track_metadata.release_name").alias("release_name"),
-            col("track_metadata.additional_info.recording_msid").alias("recording_msid"),
-            col("track_metadata.additional_info.artist_msid").alias("artist_msid"),
-            col("track_metadata.additional_info.release_msid").alias("release_msid")
+            col("musicbrainz-track-id").alias("track_id"),
+            col("track-name").alias("track_name"),
+            col("musicbrainz-artist-id").alias("artist_id"),
+            col("artist-name").alias("artist_name"),
         )
-        df_tracks = df_tracks.withColumn("id", col("recording_msid"))
+        df_tracks = df_tracks.withColumn("id", col("track_id"))
         df_tracks = df_tracks.withColumn("last_updated", current_timestamp())
+        df_tracks = df_tracks.select("id", "track_name", "track_id", "artist_name", "artist_id", "last_updated")
+        df_tracks.show(2)
         df_tracks = df_tracks.dropDuplicates(["id"])
-        df_tracks = df_tracks.select("id", "track_name", "artist_name", "release_name", "recording_msid", "artist_msid", "release_msid", "last_updated")
+        df_tracks = df_tracks.filter(col("id").isNotNull())
         self._df_tracks = df_tracks
 
     def _write_transform_to_s3(self):
@@ -34,7 +36,7 @@ class Track:
 
     def _load(self):
         df = self._df_tracks.toPandas()
-        insert_on_conflict_do_update(df=df, table_name="tracks", check_cols=["id"], batch=5000)
+        insert_on_conflict_do_update(df=df, table_name="tracks", check_cols=["id"], batch=200000)
         log.info(f'Inserted {len(df)} tracks to db')
 
     def _validate_data(self):
@@ -49,9 +51,9 @@ class Track:
         null_artist_names = df.filter(col("artist_name").isNull()).count()
         empty_artist_names = df.filter(col("artist_name") == "").count()
 
-        null_recording_ids = df.filter(col("recording_msid").isNull()).count()
+        null_recording_ids = df.filter(col("track_id").isNull()).count()
         total_rows = df.count()
-        unique_recording_ids = self._df.select("recording_msid").distinct().count()
+        unique_recording_ids = self._df.filter(col("musicbrainz-track-id").isNotNull()).select("musicbrainz-track-id").distinct().count()
 
         if null_track_names > 0:
             log.error(f"Data quality issue: {null_track_names} records have null track_name.")
@@ -68,11 +70,11 @@ class Track:
             is_valid = False
 
         if null_recording_ids > 0:
-            log.error(f"Data quality issue: {null_recording_ids} records have null recording_msid.")
+            log.error(f"Data quality issue: {null_recording_ids} records have null track_id.")
             is_valid = False
 
         if total_rows != unique_recording_ids:
-            log.error(f"Data quality issue: Duplicate recording_msids found. Total rows: {total_rows}, Unique: {unique_recording_ids}")
+            log.error(f"Data quality issue: Duplicate track_ids found. Total rows: {total_rows}, Unique: {unique_recording_ids}")
             is_valid = False
 
         if total_rows == 0:
@@ -94,6 +96,6 @@ class Track:
 
 
 if __name__ == '__main__':
-    s3_url = f"raw/{datetime.today().strftime('%Y%m%d')}/dataset.txt"
+    s3_url = f"raw/{datetime.today().strftime('%Y%m%d')}/userid-timestamp-artid-artname-traid-traname.tsv"
     track_pipeline = Track(BUCKET_NAME, s3_url)
     track_pipeline.run()
