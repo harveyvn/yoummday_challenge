cadff0c17012ec548a570e79d20b8512cc312dbe
Vuong Nguyen <vugn@future-demand.com>
2025-07-07 21:48:16 +0200

    feat: add user metadata

--- module_migration_dm/src/models/user.py
+++ module_migration_dm/src/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime, func, TIMESTAMP
+from sqlalchemy import Column, String, DateTime, func, TIMESTAMP, Integer, CHAR, Date
 from sqlalchemy.dialects.postgresql import UUID
 from .base import Base
 
@@ -8,6 +8,11 @@ class User(Base):
 
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
     user_name = Column(String(255), unique=True, nullable=False)
+
+    gender = Column(CHAR(1), nullable=True)  # 'm' or 'f'
+    age = Column(Integer, nullable=True)
+    country = Column(String(100), nullable=True)
+    registered = Column(Date, nullable=True)
 
     date_created = Column(DateTime(timezone=False), server_default=func.now())
     last_updated = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
