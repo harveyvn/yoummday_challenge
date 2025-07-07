5ae4bd867114e8b73eb4b19b2ea95ecf8569a234
Vuong Nguyen <vugn@future-demand.com>
2025-07-07 22:23:57 +0200

    feat: remove few metadata from track

--- module_migration_dm/src/models/track.py
+++ module_migration_dm/src/models/track.py
@@ -7,13 +7,15 @@ class Track(Base):
     __tablename__ = "tracks"
 
     id = Column(UUID(as_uuid=True), primary_key=True, nullable=False)
+    track_id = Column(UUID(as_uuid=True), nullable=False)
     track_name = Column(Text(), nullable=False)
-    artist_name = Column(Text(), nullable=False)
-    release_name = Column(Text(), nullable=True)
-
-    recording_msid = Column(UUID(as_uuid=True), nullable=False)
-    artist_msid = Column(UUID(as_uuid=True), nullable=False)
-    release_msid = Column(UUID(as_uuid=True), nullable=True)
+    artist_id = Column(UUID(as_uuid=True), nullable=True)
+    artist_name = Column(Text(), nullable=True)
+    # release_name = Column(Text(), nullable=True)
+    #
+    # recording_msid = Column(UUID(as_uuid=True), nullable=False)
+    # artist_msid = Column(UUID(as_uuid=True), nullable=False)
+    # release_msid = Column(UUID(as_uuid=True), nullable=True)
 
     date_created = Column(DateTime(timezone=False), server_default=func.now())
     last_updated = Column(TIMESTAMP, server_default=func.now(), onupdate=func.current_timestamp())
